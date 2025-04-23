dc8beaf47bb6f15ea30904fee61f3f8ff0dd7b2a
Yiwang5 <yiwang5@s.sfusd.edu>
2025-04-23 09:58:55 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,19 @@
 public class APLine
 {
-  /* your code here */
+  private int a,b,c;
   
+  public APLine(num1, num2, num3){
+  a = num1;
+  b = num2;
+  c = num3;
+  }
+  public double getSlope(){
+  double temp = (double) -a/b;
+  return temp;
+  }
+
+  public Boolean isOnline(x,y){
+  boolean temp = (a*x) + (a*y) + c == 0;
+  return temp;
+  }
 }
